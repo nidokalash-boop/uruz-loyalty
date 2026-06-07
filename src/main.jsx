64dbad44d428adc5dcb26a75624320915fd35eb2
nidokalash-boop <nido.kalash@gmail.com>
2026-06-07 14:50:03 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MemberPortal from './MemberPortal'
+import AdminPanel from './AdminPanel'
+import GymDisplay from './GymDisplay'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/"        element={<MemberPortal />} />
+      <Route path="/admin"   element={<AdminPanel />} />
+      <Route path="/display" element={<GymDisplay />} />
+    </Routes>
+  </BrowserRouter>
+)
